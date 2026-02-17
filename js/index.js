bf88806a26f7b961d77434279644eb2f85abe43e
Your Name <you@example.com>
2026-02-16 19:32:53 -0800

    js added, created footer and skills

---
+++ js/index.js
@@ -0,0 +1,50 @@
+
+
+// Variable Declearing portion starts 
+    const footer = document.createElement('footer');
+    const today = new Date();
+    const thisYear = today.getFullYear();
+    const copyright = document.createElement('p');
+
+    const skills = ["JavaScript", "HTML", "CSS", "Android Studio",
+                    "GitHub", "Java", "JSON", "FireBase", "XML"];
+
+    const skillsSection = document.getElementById('skills');
+    const skillsList = skillsSection.querySelector('ul'); 
+    
+    
+
+// Variable Declearing portion ends 
+
+// Styling portion start 
+
+footer.style.textAlign = "right";
+footer.style.fontSize="15px";
+footer.style.marginTop= "15px";
+footer.style.textDecoration = 'underline';
+
+
+skillsSection.style.textDecoration = 'underline';
+skillsSection.style.fontWeight = 'bold';
+skillsList.style.color = 'rgba(242, 168, 40, 1)';
+
+// Styling portion ends
+
+
+// genral operatioins 
+
+copyright.innerHTML= "© Siddhartha Biswas " +thisYear;
+footer.innerHTML = copyright.innerHTML;
+document.body.appendChild(footer);
+
+for(let i = 0; i < skills.length; i++){
+
+    const skill = document.createElement('li');
+    skill.innerHTML=skills[i];
+    skillsList.appendChild(skill);
+
+}
+
+
+
+
